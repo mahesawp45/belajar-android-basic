6453b651e834be0e42a3d6de1cd10418d64d532b
Mahesa WP <kt.mahesa@gmail.com>
2023-01-12 19:00:34 +0800

    DAY 2

--- app/src/main/java/com/example/belajarandroiddasar/MainActivity.kt
+++ app/src/main/java/com/example/belajarandroiddasar/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.belajarandroiddasar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var sayBabyButton: Button
+    private lateinit var sayBabyTextView: TextView
+
+    private fun initComp() {
+        nameEditText = findViewById(R.id.nameEditText)
+        sayBabyButton = findViewById(R.id.sayBabyButton)
+        sayBabyTextView = findViewById(R.id.sayBabyTextView)
+    }
+
+//    Ini method buat bkin tampilannya/activity tampil
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.hello_world)
+
+        initComp()
+
+        sayBabyTextView.text = resources.getString(R.string.say_baby)
+
+        sayBabyButton.setOnClickListener {
+            Log.i("MWP", "SAY BABY CLICK OWH")
+            val name = nameEditText.text.toString()
+            sayBabyTextView.text = "Hi Baby $name"
+        }
     }
 }
